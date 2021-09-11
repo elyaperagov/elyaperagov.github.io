abf040c9f23c140711d232152463a958b736e277
elyaperagov <piragof@gmail.com>
2021-09-11 19:21:37 +0300

    добавляет странички сервисов

---
+++ _nuxt/static/1631377241/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontacts","\u002Fcombined_service","\u002Fauto_service","\u002Fmarine_service","\u002Frailway_service","\u002Fcustom_service","\u002Fcontainer_service"]})
