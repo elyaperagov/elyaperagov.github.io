9d2ad7dd5c4341bfaff8d8a0d9c9a34b0ed1c01b
elyaperagov <piragof@gmail.com>
2021-09-13 14:22:42 +0300

    добавил анимацию

---
+++ _nuxt/static/1631532106/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fcontainer_service","\u002Frailway_service","\u002Fcombined_service","\u002Fcontacts","\u002Fauto_service","\u002Fcustom_service","\u002Fmarine_service"]})
