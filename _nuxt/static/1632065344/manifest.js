5841c763f015213f209d1b554fd52d9c23b7be29
elyaperagov <piragof@gmail.com>
2021-09-19 19:41:10 +0300

    для показа

--- _nuxt/static/1632065344/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fblog_page","\u002Fcombined_service","\u002Fcontainer_service","\u002Fcustom_service","\u002Fmarine_service","\u002Fprice","\u002Fproject","\u002Fseo_page","\u002Frailway_service","\u002Fauto_service","\u002Fblog","\u002Fcontacts"]})
