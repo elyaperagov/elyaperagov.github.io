e74366238cea816c9c520f949fd4523951a397ce
elyaperagov <piragof@gmail.com>
2022-02-11 19:27:50 +0300

    fc

--- send.php
+++
@@ -1,7 +0,0 @@
-<?php
-$to = "info@virtualelectronics.sg";
-$subject = "complete form on gipnetix website";
-$message = "Name: " . $_POST['name'] . "\nE-mail: " . $_POST['email'] . "\nMessage: " . $_POST['message'];
-$headers = "From: info@gipnetix.com";
-
-mail($to, $subject, $message, $headers);
