98e799ce79331cfa9cbe1d925d88f228bdfd22b6
elyaperagov <piragof@gmail.com>
2022-09-25 21:55:11 +0300

    todo

---
+++ precache-manifest.e681c2742f6ab439532a0e858d3f5bea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91fd4bddbd90371d91a4",
+    "url": "/static/css/main.7d238630.chunk.css"
+  },
+  {
+    "revision": "91fd4bddbd90371d91a4",
+    "url": "/static/js/main.91fd4bdd.chunk.js"
+  },
+  {
+    "revision": "f68049a37346add25ff0",
+    "url": "/static/js/1.f68049a3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6926ff325e6f0955b9c6d1e005d32961",
+    "url": "/index.html"
+  }
+];
